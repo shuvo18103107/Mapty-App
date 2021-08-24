708e3c5113cb629858d9e07fc5e795795800954b
shuvo18103107 <18103107@iubat.edu>
2021-08-24 19:50:57 +0600

    📝 implementing geolocation api

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//geolocation api 
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        //1st callback is success when browser get the coordinate,2nd cllback is the error confirmation
+        //success call back
+        function (position) {
+            console.log(position);
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            // console.log(latitude, longitude);
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        },
+        //error call back
+        function () {
+            alert('Could not get your position')
+        }
+    )
+}
