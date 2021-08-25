736c7360bc5f234a31b5392c6a7e4c36ac9a434b
shuvo18103107 <18103107@iubat.edu>
2021-08-25 13:34:09 +0600

    📝 Rendering workout input form

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map, mapEvent;
 //geolocation api and leaflet api implementation
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -25,7 +25,7 @@ if (navigator.geolocation) {
             //leaflet API implemtation code
             //here map is the id name where we want to show our map
             const coord = [latitude, longitude];
-            const map = L.map('map').setView(coord, 13);
+            map = L.map('map').setView(coord, 13);
             // console.log(map);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution:
@@ -37,25 +37,15 @@ if (navigator.geolocation) {
             //displaying a map marker
             //on method is not come from js , it came from leaflet api
             //here we use on method basically to add an eventlistner
-            map.on('click', function (mapEvent) {
-                // console.log(mapEvent); //this event gives us a object inside it we get lat and long now add marker via this
-                //adding marker 
-                const { lat, lng } = mapEvent.latlng
-                L.marker([lat, lng])
-                    .addTo(map)
-                    .bindPopup(L.popup({
-                        //define size of the pop up
-                        maxWidth: 250,
-                        minWidth: 100,
-                        //auto close when another place on map we click so make it false
-                        autoClose: false,
-                        //when click on cross on the pop up it disappera make it false
-                        closeOnClick: false,
-                        //we can assign any css classname on the popup
-                        className: 'running-popup'
-                    }))
-                    .setPopupContent('welcome shuvo')
-                    .openPopup();
+            map.on('click', function (mapE) {
+                //  console.log(mapE); //this event gives us a object inside it we get lat and long now add marker via this
+
+                mapEvent = mapE;
+                //show the workout form
+
+                form.classList.remove('hidden')
+                inputDistance.focus();
+
 
             })
         },
@@ -67,3 +57,33 @@ if (navigator.geolocation) {
     );
 }
 
+//form submit eventlistner
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    //clear input fields 
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+    //display marker 
+    // adding marker 
+    const { lat, lng } = mapEvent.latlng
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            //define size of the pop up
+            maxWidth: 250,
+            minWidth: 100,
+            //auto close  when another place on map we click so make it false
+            autoClose: false,
+            //when click on cross on the pop up it disappera make it false
+            closeOnClick: false,
+            //we can assign any css classname on the popup
+            className: 'running-popup'
+        }))
+        .setPopupContent('welcome shuvo')
+        .openPopup();
+})
+
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+
+})
